c34a39168aa3cd7c7671ffb3fb1fe525bf3ef8fd
Mayank Nautiyal <mayanknautiyal4ever@gmail.com>
2025-07-12 14:51:13 +0530

    Create Home.jsx

---
+++ Stackit :- A Minimal Q&A Forum Platform/Src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import QuestionList from '../components/questions/QuestionList';
+
+const Home = () => (
+  <div>
+    <h1>All Questions</h1>
+    <QuestionList />
+  </div>
+);
+
+export default Home;
